104e7e9c9c6d19186fd20e32889379aacaab997e
siddhantbarua <siddhant.barua13@gmail.com>
2024-05-27 20:46:28 +0530

    Quit with Ctrl Q

--- src/editor.rs
+++ src/editor.rs
@@ -1,17 +1,17 @@
-use crossterm::event::{read, Event::Key, KeyCode::Char};
+use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
 use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
 
 pub struct Editor {
-
+    should_quit: bool
 }
 
 impl Editor {
 
     pub fn default() -> Self {
-        Editor{}
+        Editor{should_quit: false}
     }
 
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         // terminal starts in canonical/cooked mode by default
         if let Err(err) = self.repl() {
             panic!("{err:#?}")
@@ -20,19 +20,26 @@ impl Editor {
         println!("Goodbye!");
     }
 
-    fn repl(&self) -> Result<(), std::io::Error> {
+    fn repl(&mut self) -> Result<(), std::io::Error> {
 
         enable_raw_mode()?;     // rep() signature allows us to unwrap without handling Err()
 
         loop {
-            if let Key(event) = read()? {
-                println!("{event:?} \r");
+            if let Key(KeyEvent {
+                code, modifiers, kind, state
+            }) = read()? {
+                println!("Code: {code:?}, Modifiers: {modifiers:?}, Kind: {kind:?}, State: {state:?} \r");
 
-                if let Char(c) = event.code{
-                    if c == 'q' {
-                        break;
-                    }
+                match code {
+                    Char('q') if modifiers == KeyModifiers::CONTROL => {
+                        self.should_quit = true;
+                    },
+                    _ => ()
                 }
+            }
+
+            if self.should_quit {
+                break;
             }
         }
 
